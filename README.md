6726cd883921dc48580c00b88d60e1fc95627a43
Graham B <hello@gbradley.com>
2013-08-24 23:21:12 +0100

    first commit

--- README.md
+++ README.md
@@ -1,4 +1,98 @@
 Template
 ========
 
-A PHP-based template class with mutliple inheritance
+PHP-based templating with mutliple inheritance.
+
+If you don't want to learn another syntax for templating, and prefer a lightweight class over more complex solutions, then read on.
+
+### Set up
+
+Just require the class, then create an instance passing the path to the top-level directory containing your templates.
+
+    $template = new Template('templates/)';
+    
+By default Template expects your template files to have the extension `.tpl.php`, but you can override this using the static `$extension` property.
+
+### Creating a template file
+
+Template files consist of 'blocks' in order to separate content. The first block in your template is the initialisation block, which you create by passing an anonymous function to the `init` method.
+
+    <? $this->init(function(){ ?>
+    Hello, world!
+    <? }); ?>
+    
+Save this inside your template folder (in this example we'll call it `base.tpl.php`). To render the template, just pass the name of the file (without the extension) to the `render` method:
+
+    print $template->render('base');
+    
+### Adding blocks
+
+You can define more blocks with the `define` method. This method accepts a label for the block and a closure, just like `init`.
+
+    <? $this->define('greeting', function(){ ?>
+    Hello, world!
+    <? }); ?>
+
+The first argument to every block function is the template instance. This lets you embed blocks inside other blocks, by using `insert` and passing the label of the block you want to insert:
+
+    <? $this->init(function($template){ ?>
+    <? $template->insert('greeting'); ?>
+    <? }); ?>
+    
+    <? $this->define('greeting', function(){ ?>
+    <p>Hello, world!</p>
+    <? }); ?>
+    
+### Passing arguments
+
+Passing arguments to blocks is easy; just add them to the render method after the template name:
+
+    $name = 'Graham';
+    $template->render('base', $name);
+     
+You can add as many arguments as you like, and they will be passed to every block function in sequence:
+
+    <? $this->define('greeting', function($template, $name){ ?>
+    <p>Hello, <?=$name?>!</p>
+    <? }); ?>
+    
+If you're passing more than a couple of arguments, its probably better to just pass an associative array:
+
+    $vars = array(
+        'name' 	=> 'Graham',
+        'age'	=>	32
+        );
+    $template->render('base', $vars);
+    
+### Extending a template
+
+Once you've defined a base template, you often want to be able to re-use parts of that template in other templates. By extending a template you can reuse the blocks you want, and define or override others.
+
+To extend a template, create a new file (let's call it 'signin.tpl.php') and use the `extend` method to denote which template you're extending.
+
+    <? $this->extend('base'); ?>
+    
+Because you're extending a template with an `init` method, you can skip that step and go straight to defining your blocks. You can easily overwrite an existing block:
+
+    <? $this->define('greeting', function($template, $vars){ ?>
+    <p>Hello, <?=$vars['name']?>, sign in now!</p>
+    <? }); ?>
+    
+Alternatively you can avoid duplicating the block's content with `super`, which inserts the extended template's block:
+
+    <? $this->define('greeting', function($template, $vars){ ?>
+    <? $template->super(); ?>
+    <p>Why don't you sign in now.</p>
+    <? }); ?>
+    
+    
+### Multiple inheritance
+
+Once you've created a set of templates, you may find that you want a new template which borrows some blocks from one template, and some blocks from another.
+
+To do this, just pass the names of both templates to `extend`:
+
+    <? $this->extend('templateA', 'templateB'); ?>
+    
+The order of the arguments denotes the precedence if both templates define blocks with the same label.
+
---
+++ Template.php
@@ -0,0 +1,201 @@
+<?php
+
+/**
+* Native PHP templating with multiple inheritance
+**/
+
+class Template {
+
+	protected $tree 			= null;				// template inheritance tree
+	protected $options  		= null;
+	private $current_tpl 		= null;				// the current template
+	private $current_label 		= null;				// the current block label
+
+	public static $extension 	= '.tpl.php';		// the template extension
+	protected static $closures 	= null;				// stores closures for blocks
+
+
+	/**
+	* @param string $path path to template directory
+	**/
+	public function __construct($path) {
+		$this->path = $path;
+	}
+
+	/**
+	* Adds templates to the inheritance tree.
+	* 
+	* @param string $tpl template name
+	* @param string $multi_tpl... define more templates for multiple inheritance
+	**/
+	public function extend() {
+		$this->tree[] = func_get_args();
+	}
+	
+	/**
+	* Insert a block.
+	* 
+	* @param string $label The label of the block to insert
+	* @param mixed $args (optional) Any extra arguments get added to the arguments array from render() and passed into the block.
+	**/
+	public function insert() {
+
+		$args = func_get_args();
+		$label = array_shift($args);
+
+		$tmp = $this->current_label;								// store the name of the current insert, then override with the new one
+		$this->current_label = $label;
+
+		foreach ($this->tree as $branch) {							// loop through the tree until we find a match
+			foreach ($branch as $tpl) {
+				if ($this->runBlock($label, $tpl, $args)) {
+					break 2;
+				}
+			}
+		}
+		$this->current_label = $tmp;								// revert back to old insert name
+	}
+	
+	/**
+	* Generate and return the template output.
+	* 
+	* @param string $tpl (optional) the template name to render
+	* @param mixed $args (optional) Any arguments get passed into all insert functions.
+	* @return string HTML output
+	**/
+	public function render() {
+
+		self::$closures = array();
+	
+		$args = func_get_args();										// grab all arguments and convert template to path format
+		$tpl = implode('/', array_map('strtolower', preg_split('/(?=[A-Z])/', array_shift($args), -1, PREG_SPLIT_NO_EMPTY)));
+		$this->tree = array(array(str_replace(self::$extension, '', $tpl)));
+			
+		$template = &$this;												// store a reference to the instance, and add it to the args array
+		array_unshift($args, $this);
+		$template->vars = $args;
+
+		$i = 0;
+		ob_start();
+		while ($i < count($this->tree)) {								// work up through the template tree
+			$branch = $this->tree[$i];
+			$j = 0;
+			while ($j < count($branch)){								// work through the branch
+				$this->current_tpl = $branch[$j];
+				require_once($this->path . self::pathForTemplate($this->current_tpl));
+				$j++;
+			}
+			$i++;
+		}
+		ob_clean();
+		$this->runBlock();
+		$contents = ob_get_contents();
+		ob_end_clean();
+
+		self::$closures = null;
+		return $contents;
+	}
+	
+	/**
+	* When called from inside a block, inserts the corresponding block from the closest ancestor or sibling template.
+	**/
+	public function super() {
+		foreach ($this->tree as $i => $branch) {						// loop through tree and find the position of the current template
+			$index = array_search($this->current_tpl, $branch);
+			if ($index !== false && $i < count($this->tree) - 1) {		// check the template isn't at the top of the tree
+				for ($j = $i + 1; $j < count($this->tree); $j++) {		// work upwards through the remaining groups
+					$group = $this->tree[$j];
+					foreach ($group as $tpl) {							// work sideways through each group
+						if ($this->runBlock($this->current_label, $tpl)) {
+							break 3;
+						}
+					}
+				}
+			}
+		}
+	}
+
+	/**
+	* Convert characters to their HTML entity equivalent.
+	*
+	* @param string $value
+	* @param string
+	**/
+	public function escape($value) {
+		return htmlentities($value, ENT_COMPAT, 'UTF-8');
+	}
+
+	/**
+	* Register the initiialise block.
+	*
+	* @param function $closure The block closure
+	**/
+	public function init($closure) {
+		self::$closures['*'] = $closure;
+	}
+
+	/**
+	* Register a block.
+	*
+	* @param string $label The block label
+	* @param function $closure The block closure
+	**/
+	public function define($label, $closure) {
+		self::$closures[$this->current_tpl .'_' . $label] = $closure;
+	}
+
+	/**
+	* Finds and runs a block, passing through any optional arguments
+	*
+	* @param string $label (optional) The block label, or null for the initialise block
+	* @param string $tpl (optional) The name of a template
+	* @param array $pass_thru (optional) Array of arguments to pass sequentially to the block
+	* @return boolean
+	**/
+	private function runBlock($label = null, $tpl = null, $pass_thru = null) {
+		$block = self::getBlock($label ? ($tpl . '_' . $label) : '*');
+		if ($block) {
+			$args = $this->vars;
+			if ($pass_thru) {
+				$args = array_merge($args, $pass_thru);
+			}
+			call_user_func_array($block, $args);
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	/**
+	* Return the path for a template name
+	*
+	* @param string $tpl
+	* @return string
+	**/
+	public static function pathForTemplate($tpl) {
+		return $tpl . self::$extension;
+	}
+
+	/**
+	* Return the template name for a path
+	*
+	* @param string $path
+	* @return string
+	**/
+	public static function templateForPath($path) {
+		return str_replace(self::$extension, '', $path);
+	}
+
+	/**
+	* Aceepts a label and returns the block closure if found.
+	*
+	* @param string $blockName
+	* @return function
+	**/
+	private static function getBlock($blockName) {
+		return isset(self::$closures[$blockName]) ? self::$closures[$blockName] : null;
+	}
+
+}
+
+?>
